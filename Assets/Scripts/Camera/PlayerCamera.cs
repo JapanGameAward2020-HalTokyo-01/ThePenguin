cc12c98730b9a1416eda345f06aa2ff4a442e461
yuanyongqi <moc403038240@gmail.com>
2020-03-12 15:49:24 +0900

    En Yuki
    固定カメラ、追尾カメラ

---
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -0,0 +1,60 @@
+﻿/**
+* @file     PlayerCamera.cs
+* @brief    カメラ
+* @author   En Yuki
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerCamera : MonoBehaviour
+{
+    //固定カメラか、追尾カメラか
+    public bool m_isFixed = true;
+    //追尾オブジェクト
+    public GameObject m_focusObject;
+    //追尾スピード(0-1.0)
+    public float m_speedScale = 0.075f;
+    //カメラ注視座標（追尾オブジェクトが設立されていない場合のみ有効）
+    public Vector3 m_focus = Vector3.zero;
+
+    //Transformコンポーネント
+    private Transform m_trans;
+    //初期位置計算用距離
+    private float m_length = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_trans = GetComponent<Transform>();
+
+        //追尾オブジェクトからfocusを取得
+        if (m_focusObject!=null)
+        {
+            m_focus = m_focusObject.GetComponent<Transform>().position;
+        }
+
+
+        //初期位置を設定
+        m_trans.SetPositionAndRotation(new Vector3(m_focus.x + m_length, m_focus.y + m_length, m_focus.z - m_length), Quaternion.identity);
+        //Rotationを設定
+        m_trans.LookAt(m_focus, Vector3.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //追尾カメラ
+        if (!m_isFixed && m_focusObject != null)
+        {
+            //追尾オブジェクトから座標計算
+            Vector3 temp = m_focusObject.GetComponent<Transform>().position;
+            m_focus = m_focus+(temp-m_focus)* m_speedScale;
+
+            //位置を設定
+            m_trans.SetPositionAndRotation(new Vector3(m_focus.x + m_length, m_focus.y + m_length, m_focus.z - m_length), m_trans.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/TestMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Transform>().position += new Vector3(0, 0, 0.04f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Transform>().position += new Vector3(0, 0, -0.04f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Transform>().position += new Vector3(-0.04f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Transform>().position += new Vector3(0.04f, 0, 0);
+        }
+    }
+}

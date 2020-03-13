c89aafd2b57d70be3614d90d8b94128a26d98a22
yuanyongqi <moc403038240@gmail.com>
2020-03-13 14:16:59 +0900

    ゴールタイル
    子ペンギン、親ペンギンのいずれかがゴールに接触すればクリアするタイル

---
+++ Assets/Scripts/GoalTile.cs
@@ -0,0 +1,45 @@
+﻿/**
+* @file     GoalTile.cs
+* @brief   ゴールタイル
+* @author   En Yuki
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Trigger
+    void OnTriggerEnter(Collider other)
+    {
+        //親ペンギンと子ペンギンにのみ反応する
+        if (other.GetComponentInParent<ParentPenguinMove>() != null || other.GetComponentInParent<ChildPenguinMove>() != null)
+        {
+            Goal();
+        }
+    }
+
+    /**
+    * @brief    ゴール処理
+    * @param(viod)   なし
+    * @return   None
+    */
+    void Goal()
+    {
+        //仮処理
+        Debug.Log("Goal");
+    }
+}

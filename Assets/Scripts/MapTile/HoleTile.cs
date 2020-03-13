5ff5e38f3b611dee4dc10dd59f8f682a39e83a02
tekkii <tekkii104800@icloud.com>
2020-03-13 19:04:50 +0900

    穴タイル実装。マップタイルフォルダ整理

---
+++ Assets/Scripts/MapTile/HoleTile.cs
@@ -0,0 +1,38 @@
+﻿/**
+* @file     HoleTile.cs
+* @brief    穴タイル
+* @author   Taku Hekisen
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class HoleTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ペンギンが穴に落ちたらの処理
+    private void OnTriggerEnter(Collider other)
+    {
+        //トリガーに当たったオブジェクトのTagがPlayerだったら
+        if(other.tag=="Player")
+        {
+            //仮処理
+            Debug.Log("Drop");
+        }
+    }
+}

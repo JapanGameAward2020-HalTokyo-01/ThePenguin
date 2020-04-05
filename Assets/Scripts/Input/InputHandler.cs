93e3f60844e2a317c5aca8c815ba45e0f7122e86
MIYAMASA362 <MIYAMASA362@yahoo.co.jp>
2020-04-06 04:48:45 +0900

    InputHandlerの可読性向上 InputModuleを設計

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 /// <summary>
 /// @class InputSystem
 /// @brief コントローラー入力処理
+///        InputModuleによってパワーなどが操作される
 /// </summary>
 public class InputHandler : MonoBehaviour
 {
@@ -17,65 +18,45 @@ public class InputHandler : MonoBehaviour
         Run
     }
 
-    [SerializeField]
-    private ParentPenguinMove m_ParentPenguin;
+    private InputModuleBase m_InputModule;
 
-    [SerializeField,Tooltip("ゲージのリピート")]
-    private bool m_IsReperat = true;
-
-    private float m_Power = 0f;
-
-    [SerializeField,Tooltip("最大値"),Range(1f, 10f)]
+    //! パワー最大値
+    [SerializeField, Tooltip("最大値"), Range(1f, 10f)]
     private float m_PowerMax = 10f;
+    public float PowerMax
+    {
+        get { return m_PowerMax; }
+    }
 
-    [SerializeField,Tooltip("毎秒変化値(×フレーム時間)"),Range(0.01f, 10.0f)]
+    //! パワー変化量
+    [SerializeField, Tooltip("毎秒変化値(×フレーム時間)"), Range(0.01f, 10.0f)]
     private float m_PowerChange = 3.0f;
 
-    [SerializeField,Tooltip("待機時間")]
-    private float m_PowerWaitTime = 10.0f;
+    //! パワー
+    public float Power { get; set; } = 0f;
 
-    [Header("入力パラメータ")]
-
-    [SerializeField,Tooltip("無効範囲"),Range(0.0f, 1.0f)]
-    private float m_Deadzone = 0.07f;
-
-    [SerializeField,Tooltip("誤差しきい値"),Range(0.0f, 1.0f)]
-    private float m_Threshold = 0.1f;
-
-    //デバッグ系
-    [Space(8)]
+    //! 状態
+    public State CurrentState { get; private set; } = State.Idle;
+    //! 入力ベクタ
+    public Vector3 InputVector { get; private set; } = Vector3.zero;
 
     [SerializeField]
     private GameObject m_Arrow;
-
     [SerializeField]
     private GameObject m_DemoObject;
-
-
-
-    //状態
-    private State m_State = State.Idle;
-    public State CurrentState
-    {
-        get { return m_State; }
-    }
-
-    private float m_Horizontal = 0f;
-
-    private float m_Vertical = 0f;
-
-    //移動向きとして利用される 入力ベクトル
-    private Vector3 m_InputVector;
-
-    private Vector3 m_CurrentInputVector;
-
-    private float m_TimeCounter = 0f;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        TimeCounterReset();
+        //! 有効なModuleを探査/代入
+        foreach(var module in GetComponents<InputModuleBase>())
+        {
+            if (!module.enabled) continue;
+            m_InputModule = module;
+            m_InputModule.Handler = this;
+            break;
+        }
+
         if(m_Arrow != null)
             m_Arrow.SetActive(false);
     }
@@ -83,110 +64,71 @@ public class InputHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (this.CurrentState == State.Run)
-            Run();
-        else
-            Idle();
-
-        DebugFunc();
-    }
-
-    /// <summary>
-    /// @brief  入力を拒否する状態
-    /// </summary>
-    void Run()
-    {
-        m_ParentPenguin.MoveInputVector(GetMoveVector());
-        m_Power -= m_PowerChange * Time.deltaTime;
-
-        if (m_Power > 0f) return;
-
-        this.ChangeState(State.Idle);
-        this.PowerReset();
-    }
-
-    /// <summary>
-    /// @brief  入力待ち状態
-    ///     入力が無い場合 : チャージしていればRunに遷移
-    ///     入力がある場合 : 前の入力との状態を見て、チャージ開始
-    /// </summary>
-    void Idle()
-    {
-        //入力
-        m_Horizontal = Input.GetAxis("Horizontal");
-        m_Vertical = Input.GetAxis("Vertical");
-
-        m_CurrentInputVector = TransformCameraDirection(new Vector3(m_Horizontal, 0f, m_Vertical));
-        m_CurrentInputVector.y = 0f;
-
-        if (Mathf.Abs(m_Horizontal) <= m_Deadzone && Mathf.Abs(m_Vertical) <= m_Deadzone)
+        if (CurrentState == State.Run)
         {
-            this.TimeCounterReset();
+            Power -= m_PowerChange * Time.deltaTime;
 
-            //チャージ終了
-            if (m_Power > 0f)
-            {
-                this.ChangeState(State.Run);
-            }
-            return;
-        }
+            if (Power > 0f) return;
 
-        if (m_InputVector != m_CurrentInputVector && EnableInputVector(m_CurrentInputVector))
-        {
-            m_InputVector = m_CurrentInputVector;
-            this.TimeCounterReset();
-            this.PowerReset();
+            ChangeState(State.Idle);
+            PowerReset();
         }
         else
         {
-            m_TimeCounter++;
-
-            if (m_TimeCounter < m_PowerWaitTime) return;
-
-            m_Power += m_PowerChange * Time.deltaTime;
-
-            if (m_Power > m_PowerMax) m_Power = m_IsReperat ? 0f : m_PowerMax;
+            m_InputModule.Behaviour();
         }
+
+        DebugMethod();
     }
 
     /// <summary>
-    /// @brief
-    ///     デバッグ用の表示関数
-    /// @param()
-    /// @return
+    /// @brief パワーを0にする
     /// </summary>
-    void DebugFunc()
+    public void PowerReset()
     {
-        Debug.DrawRay(this.transform.position, Camera.main.transform.forward, Color.red);
-        Debug.DrawRay(this.transform.position, m_CurrentInputVector, Color.green);
-        Debug.DrawRay(this.transform.position, GetMoveVector(), Color.blue);
-
-        if (m_Arrow != null)
-        {
-            m_Arrow.SetActive(true);
-            m_Arrow.transform.rotation = Quaternion.LookRotation(m_InputVector);
-        }
-
-        if (m_DemoObject != null)
-        {
-            if (CurrentState == State.Idle)
-            {
-                m_DemoObject.SetActive(m_Power != 0f);
-                if(m_Power == 0f)
-                {
-                    m_DemoObject.transform.localPosition = Vector3.zero;
-                    m_DemoObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-                }
-                m_DemoObject.GetComponent<Rigidbody>().AddForce(GetMoveVector(),ForceMode.VelocityChange);
-                
-            }
-            else
-            {
-                m_DemoObject.SetActive(m_Power != 0f);
-            }
-        }
+        Power = 0f;
     }
 
+    /// <summary>
+    /// @brief 移動ベクトルの取得
+    /// </summary>
+    public Vector3 GetMoveVector()
+    {
+        return -Vector3.Normalize(InputVector) * Power;
+    }
+
+    /// <summary>
+    /// @brief 
+    ///     状態の切り替え
+    ///     
+    /// </summary>
+    public void ChangeState(State state)
+    {
+        CurrentState = state;
+    }
+
+    /// <summary>
+    /// @brief 
+    ///     _InputVectorを入力ベクトルに設定
+    ///     
+    /// </summary>
+    public void SetInputVector(Vector3 _InputVector)
+    {
+        _InputVector.y = 0f;
+        this.InputVector = _InputVector;
+    }
+
+    /// <summary>
+    /// @brief 
+    ///     m_PowerChargeの値でパワー値を変化
+    /// @param(IsRepeat) 最大値を超えた場合、0に戻すか (true:戻す/false:戻さない)
+    /// </summary>
+    public void PowerAddChange(bool IsRepeat = false)
+    {
+        Power += m_PowerChange * Time.deltaTime;
+        if (Power > m_PowerMax)
+            Power = IsRepeat ? 0f : m_PowerMax;
+    }
 
     /// <summary>
     /// @brief 
@@ -196,7 +138,7 @@ public class InputHandler : MonoBehaviour
     /// @param(dir) 変換する向き
     /// @return 変換した向き
     /// </summary>
-    Vector3 TransformCameraDirection(Vector3 dir)
+    public Vector3 TransformCameraDirection(Vector3 dir)
     {
         Vector3 _result = Vector3.zero;
 
@@ -208,50 +150,32 @@ public class InputHandler : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// @brief
+    ///     デバッグ用の表示関数
+    /// @param()
+    /// @return
     /// </summary>
-    void PowerReset()
+    void DebugMethod()
     {
-        m_Power = 0f;
+        Debug.DrawRay(this.transform.position, Camera.main.transform.forward, Color.red);
+        Debug.DrawRay(this.transform.position, GetMoveVector(), Color.blue);
+
+        if (m_Arrow != null)
+        {
+            m_Arrow.SetActive(true);
+            m_Arrow.transform.rotation = Quaternion.LookRotation(InputVector);
+        }
+
+        if (m_DemoObject != null)
+        {
+            m_DemoObject.SetActive(Power != 0f);
+
+            if (CurrentState == State.Idle)
+                m_DemoObject.transform.localPosition = GetMoveVector();
+            
+        }
+
+        m_InputModule.DebugMethod();
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    void TimeCounterReset()
-    {
-        m_TimeCounter = 0f;
-    }
-
-    /// <summary>
-    /// @brief 
-    /// </summary>
-    Vector3 GetMoveVector()
-    {
-        m_InputVector.y = 0f;
-        return -Vector3.Normalize(m_InputVector) * m_Power;
-    }
-
-    /// <summary>
-    /// @brief 
-    ///     入力値の誤差判定
-    ///     
-    /// @param(_InputVector) 現在の入力値
-    /// @return しきい値を超えた誤差かの真偽
-    /// </summary>
-    bool EnableInputVector(Vector3 _InputVector)
-    {
-        Vector3 diffrent = m_InputVector - _InputVector;
-        return (Mathf.Abs(diffrent.x) > m_Threshold) && (Mathf.Abs(diffrent.z) > m_Threshold);
-    }
-
-    /// <summary>
-    /// @brief 
-    ///     状態の切り替え
-    ///     
-    /// </summary>
-    void ChangeState(State state)
-    {
-        m_State = state;
-    }
 }

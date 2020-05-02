e3412178b67f04130c0f8eee59dfacdbc783c499
Mizuki_Tanisawa <wasaizumi@gmail.com>
2020-05-02 16:58:21 +0900

    BGM鳴らすだけ(AudioManager), AudioManagerの追加機能(AudioSystem群)として区間ループ(AudioLoop)作成
    ループの精度なんかいまいち...

---
+++ Assets/Scripts/AudioSystem/AudioFactor/AudioLoop.cs
@@ -0,0 +1,43 @@
+﻿/**
+ * @file    AudioLoop.cs
+ * @brief   AudioSystem機能群：区間ループ機能
+ * @author  谷沢 瑞己
+ */
+using System;
+using UnityEngine;
+
+/**
+ * @struct  AudioLoopParam構造体
+ * @brief   機能呼び出し側で持つべきパラメータ
+ */
+[Serializable]
+public struct AudioLoopParam
+{
+	//! ループ開始サンプル数
+	[SerializeField]
+	public int m_begin;
+
+	//! ループ終了サンプル数(0の時ループしない)
+	[SerializeField]
+	public int m_end;
+}
+
+
+/**
+ * @class   AudioLoopクラス
+ * @brief   再生中のAudioSourceから現在サンプル数(位相)を取得し、操作する
+ */
+public class AudioLoop
+{
+	/**
+	 * @brief   (Update()呼び出し)区間ループチェック
+	 */
+	public void OnLoopCheck(AudioSource source, AudioLoopParam param)
+	{
+		if (param.m_end == 0) return;
+
+		// 再生区間オーバーのサンプル値に至った場合区間開始位置に戻す
+		if (source.timeSamples > param.m_end)
+			source.timeSamples = param.m_begin;
+	}
+}
---
+++ Assets/Scripts/AudioSystem/AudioManager.cs
@@ -0,0 +1,55 @@
+﻿/**
+ * @file    AudioManager.cs
+ * @brief   AudioSystem機能群とそれを使うクラスに分離し、使う側のクラスをゲームオブジェクトにくっつけるという実装方針(GoF的にはBridgeパターンをイメージ)
+ * @author  谷沢 瑞己
+ */
+using UnityEngine;
+using UnityEngine.Audio;
+
+/**
+ * @class   AudioManagerクラス
+ * @brief   AudioSystem機能群を集約し使うクラス
+ *			本体はBGMを再生する機能のみを持つ
+ */
+public class AudioManager : MonoBehaviour
+{
+	//! 操作対象オーディオクリップ
+	[SerializeField]
+	private AudioClip m_clip = null;
+
+	//! 適用するミキサー
+	[SerializeField]
+	private AudioMixerGroup m_mixer = null;
+
+	//! 操作対象オーディオソース
+	AudioSource m_source = null;
+
+	//! ループの為のパラメータ
+	[SerializeField]
+	private AudioLoopParam m_loop;
+	private AudioLoop m_loop_factor = new AudioLoop();
+
+	/**
+	 * @brief   初期化
+	 */
+	public void Awake()
+	{
+		// AudioSource作成
+		m_source = gameObject.GetComponent<AudioSource>();
+		if (m_source == null) m_source = gameObject.AddComponent<AudioSource>();
+
+		m_source.clip = m_clip;
+		m_source.outputAudioMixerGroup = m_mixer;
+		m_source.loop = true;
+		m_source.Play();
+	}
+
+	/**
+	 * @brief   更新
+	 */
+	public void FixedUpdate()
+	{
+		if(m_loop_factor != null) m_loop_factor.OnLoopCheck(m_source, m_loop);
+	}
+
+}

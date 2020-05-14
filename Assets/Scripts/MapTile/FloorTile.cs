35c4fb1bcbb1a903c042f0ec052f1a7c123b85fd
xiaoyao1222 <b1968620@gmail.com>
2020-05-15 00:35:46 +0900

    テクスチャーを切り替え用のスクリプタブルオブジェクトを作成、FloorTile.CSを作成、砂漠テクスチャー導入

---
+++ Assets/Scripts/MapTile/FloorTile.cs
@@ -0,0 +1,35 @@
+﻿/**
+* @file     FloorTile.cs
+* @brief    フロアタイル
+* @author   李　爾捷
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTile : MonoBehaviour
+{
+    public enum FieldType
+    {
+        SNOW = 0,
+        DESERT,
+
+    }
+
+    [SerializeField]
+    FieldType m_Type;
+
+    [SerializeField]
+    TextureData m_Data;
+
+    [SerializeField]
+    MeshRenderer m_Mesh;
+
+    private void OnDrawGizmos()
+    {
+        Material m = m_Mesh.sharedMaterial;
+        m.SetTexture("_Albedo", m_Data.GetTexture((int)m_Type));
+    }
+
+}

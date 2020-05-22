1cbf067fdce82248cc9cc16d6dac2cb3c5e1291e
yuanyongqi <moc403038240@gmail.com>
2020-05-22 17:44:43 +0900

    Mainカメラ修正
    Cinemachineの導入につき、カメラ修正を行いました。

--- Assets/Scripts/Camera/CameraSystem.cs
+++ Assets/Scripts/Camera/CameraSystem.cs
@@ -1,5 +1,5 @@
 ﻿/**
-* @file     PlayerCamera.cs
+* @file     CameraSystem.cs
 * @brief    カメラ
 * @author   En Yuki
 */
@@ -11,6 +11,7 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    /*
     //カメラ初期距離
     public float m_InitDistance=20.0f;
     //カメラ初期角度
@@ -30,13 +31,13 @@ public class PlayerCamera : MonoBehaviour
     private Transform m_trans;
     //cameraコンポーネント
     private Camera m_camera;
-    
+    */
 
 
     // Start is called before the first frame update
     void Start()
     {
-        m_trans = GetComponent<Transform>();
+        /*m_trans = GetComponent<Transform>();
         m_camera = GetComponent<Camera>();
 
         //追尾オブジェクトからfocusを取得
@@ -48,14 +49,14 @@ public class PlayerCamera : MonoBehaviour
         //初期位置を設定
         m_trans.SetPositionAndRotation(new Vector3(m_focus.x + m_InitDistance * Mathf.Cos(Mathf.Deg2Rad * m_InitAngle_Y) * Mathf.Sin(Mathf.Deg2Rad * m_InitAngle_XZ), m_focus.y + m_InitDistance * Mathf.Sin(Mathf.Deg2Rad * m_InitAngle_Y), m_focus.z - m_InitDistance * Mathf.Cos(Mathf.Deg2Rad * m_InitAngle_Y) * Mathf.Cos(Mathf.Deg2Rad * m_InitAngle_XZ)), Quaternion.identity);
         //Rotationを設定
-        m_trans.LookAt(m_focus, Vector3.up);
+        m_trans.LookAt(m_focus, Vector3.up);*/
     }
 
     // Update is called once per frame
     void Update()
     {
         //追尾カメラ
-        if (!m_isFixed && m_focusObject != null)
+        /*if (!m_isFixed && m_focusObject != null)
         {
             //追尾オブジェクトから座標計算
             Vector3 temp = m_focusObject.GetComponent<Transform>().position;
@@ -65,6 +66,6 @@ public class PlayerCamera : MonoBehaviour
         //位置を設定
         m_trans.SetPositionAndRotation(new Vector3(m_focus.x+ m_InitDistance * Mathf.Cos(Mathf.Deg2Rad * m_InitAngle_Y)*Mathf.Sin(Mathf.Deg2Rad*m_InitAngle_XZ), m_focus.y+ m_InitDistance * Mathf.Sin(Mathf.Deg2Rad*m_InitAngle_Y),m_focus.z- m_InitDistance * Mathf.Cos(Mathf.Deg2Rad * m_InitAngle_Y) * Mathf.Cos(Mathf.Deg2Rad * m_InitAngle_XZ)), Quaternion.identity);
         //Rotationを設定
-        m_trans.LookAt(m_focus, Vector3.up);
+        m_trans.LookAt(m_focus, Vector3.up);*/
     }
 }
---
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -0,0 +1,51 @@
+﻿/**
+* @file     MainCamera.cs
+* @brief    カメラ
+* @author   En Yuki
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Cinemachine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField]
+    //カメラ初期距離
+    private float m_InitDistance = 20.0f;
+    //カメラ初期角度
+    [SerializeField]
+    private int m_InitAngle_XZ = 0;
+    [SerializeField]
+    private int m_InitAngle_Y = 45;
+    //追尾オブジェクト
+    [SerializeField]
+    private GameObject m_focusObject;
+
+    private CinemachineVirtualCamera[] v_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v_camera=GetComponentsInChildren<CinemachineVirtualCamera>();
+        v_camera[0].LookAt = m_focusObject.transform;
+        v_camera[1].Follow = m_focusObject.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnValidate()
+    {
+        v_camera = GetComponentsInChildren<CinemachineVirtualCamera>();
+
+        v_camera[1].GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y =m_InitDistance*Mathf.Sin(Mathf.Deg2Rad*m_InitAngle_Y);
+        v_camera[0].GetCinemachineComponent<CinemachineOrbitalTransposer>().m_FollowOffset.z = -1.0f * m_InitDistance * Mathf.Cos(Mathf.Deg2Rad * m_InitAngle_Y);
+        v_camera[0].GetCinemachineComponent<CinemachineOrbitalTransposer>().m_Heading.m_Bias = m_InitAngle_XZ;
+    }
+}

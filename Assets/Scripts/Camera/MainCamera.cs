32cad212d72dd1010212db4a6d7efaca24b772fd
yuanyongqi <moc403038240@gmail.com>
2020-05-26 23:53:11 +0900

    カメラリセット関数

--- Assets/Scripts/Camera/MainCamera.cs
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -37,18 +37,27 @@ public class MainCamera : MonoBehaviour
     [SerializeField]
     private bool m_RotateReverse = false;
 
+    //Reset
+    [SerializeField]
+    private float m_ResetSpeed = 360.0f;
+
     //
     private bool m_IsSwitch = false;
     private float m_SwitchTimer = 0.0f;
     private float m_CurrentFov;
     private float m_PastFov;
+    private bool m_IsReset = false;
+    private float m_ResetTime;
+    private float m_ResetTimer = 0.0f;
+    private float m_PastAngle;
+    private float m_CurrentAngle;
 
     private CinemachineVirtualCamera[] v_camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        v_camera=GetComponentsInChildren<CinemachineVirtualCamera>();
+        v_camera = GetComponentsInChildren<CinemachineVirtualCamera>();
         v_camera[0].LookAt = m_focusObject.transform;
         v_camera[1].Follow = m_focusObject.transform;
 
@@ -61,24 +70,31 @@ public class MainCamera : MonoBehaviour
     void Update()
     {
         //動作確認用
-        if(Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            FovSwitch();
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            RotateCamera(true);
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            RotateCamera(false);
+            LookToVec();
         }
 
+        //カメラリセット
+        if (m_IsReset)
+        {
+            m_ResetTimer += Time.deltaTime;
+            float t = m_ResetTimer / m_ResetTime;
+            if (t >= 1.0f)
+            {
+                t = 1.0f;
+                m_IsReset = false;
+            }
+            m_InitAngle_XZ = m_PastAngle + t * (m_CurrentAngle - m_PastAngle);
+            v_camera[0].GetCinemachineComponent<CinemachineOrbitalTransposer>().m_Heading.m_Bias = m_InitAngle_XZ;
+        }
+
+        //Fov切り替え
         if (m_IsSwitch)
         {
             m_SwitchTimer += Time.deltaTime;
             float t = m_SwitchTimer / m_SwitchTime;
-            if(t>=1.0f)
+            if (t >= 1.0f)
             {
                 t = 1.0f;
                 m_IsSwitch = false;
@@ -91,7 +107,7 @@ public class MainCamera : MonoBehaviour
     {
         v_camera = GetComponentsInChildren<CinemachineVirtualCamera>();
 
-        v_camera[1].GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y =m_InitDistance*Mathf.Sin(Mathf.Deg2Rad*m_InitAngle_Y);
+        v_camera[1].GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset.y = m_InitDistance * Mathf.Sin(Mathf.Deg2Rad * m_InitAngle_Y);
         v_camera[0].GetCinemachineComponent<CinemachineOrbitalTransposer>().m_FollowOffset.z = -1.0f * m_InitDistance * Mathf.Cos(Mathf.Deg2Rad * m_InitAngle_Y);
         v_camera[0].GetCinemachineComponent<CinemachineOrbitalTransposer>().m_Heading.m_Bias = m_InitAngle_XZ;
     }
@@ -104,7 +120,7 @@ public class MainCamera : MonoBehaviour
             m_IsSwitch = true;
             m_SwitchTimer = 0.0f;
             m_PastFov = v_camera[0].m_Lens.FieldOfView;
-            if(m_CurrentFov==m_Fov_1)
+            if (m_CurrentFov == m_Fov_1)
             {
                 m_CurrentFov = m_Fov_2;
             }
@@ -116,10 +132,10 @@ public class MainCamera : MonoBehaviour
     }
 
     //回転関数
-    public void RotateCamera(bool direction,float scale=1.0f)
+    public void RotateCamera(bool direction, float scale = 1.0f)
     {
         float dir;
-        if((direction && !m_RotateReverse)||(!direction&&m_RotateReverse))
+        if ((direction && !m_RotateReverse) || (!direction && m_RotateReverse))
         {
             dir = 1.0f;
         }
@@ -127,7 +143,52 @@ public class MainCamera : MonoBehaviour
         {
             dir = -1.0f;
         }
-        m_InitAngle_XZ -= scale * dir * m_RotateSpeed*Time.deltaTime;
+        m_InitAngle_XZ -= scale * dir * m_RotateSpeed * Time.deltaTime;
         v_camera[0].GetCinemachineComponent<CinemachineOrbitalTransposer>().m_Heading.m_Bias = m_InitAngle_XZ;
+    }
+
+    //向き指定関数
+    public void LookToVec()
+    {
+        //ParentPenguinの作り方に依存※※
+        LookToVec(m_focusObject.GetComponentInChildren<SkinnedMeshRenderer>().gameObject.transform.forward);
+    }
+    public void LookToVec(Vector3 vec)
+    {
+        if (m_IsReset)
+        {
+            return;
+        }
+
+        //単位化
+        vec.y = 0.0f;
+        vec = Vector3.Normalize(vec);
+
+        float sita = Mathf.Acos(Vector3.Dot(vec, Vector3.forward)) * Mathf.Rad2Deg;
+
+        if (vec.x < 0)
+        {
+            sita = 360 - sita;
+        }
+
+        float current_angle = ((m_InitAngle_XZ % 360) + 360) % 360;
+
+        float angle = (sita - current_angle);
+        if (angle < -180)
+        {
+            angle += 360;
+        }
+        if (angle > 180)
+        {
+            angle -= 360;
+        }
+
+        m_PastAngle = m_InitAngle_XZ;
+        m_CurrentAngle = m_InitAngle_XZ + angle;
+
+        m_ResetTime = Mathf.Abs(angle) / m_ResetSpeed;
+
+        m_IsReset = true;
+        m_ResetTimer = 0.0f;
     }
 }

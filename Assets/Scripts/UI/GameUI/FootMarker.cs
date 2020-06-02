ed888ded61e3ffe8b4dc270f8b52f3b946894bd0
Mizuki_Tanisawa <wasaizumi@gmail.com>
2020-06-02 22:54:53 +0900

    足元マーカー修正(入力がないとき矢を消す)

--- Assets/Scripts/UI/GameUI/FootMarker.cs
+++ Assets/Scripts/UI/GameUI/FootMarker.cs
@@ -3,10 +3,9 @@
  * @brief	親ペンギンの足元のマーカー
  * @auther	谷沢　瑞己
  */
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 /**
  * @file	FootMarkerクラス
@@ -16,6 +15,9 @@ public class FootMarker : MonoBehaviour
 {
 	[SerializeField, NonEditableField, Tooltip("インプット")]
 	private PlayerInput m_input = null;
+
+	[SerializeField, Tooltip("矢の部分")]
+	private SpriteRenderer m_arrow = null;
 
 	public void Awake()
 	{
@@ -30,12 +32,27 @@ public class FootMarker : MonoBehaviour
 	// 選んだステージ読み込む
 	private void CursorRotate(InputAction.CallbackContext _context)
 	{
-		Vector3 _pos = transform.position;
 		Vector2 _in_dir = _context.ReadValue<Vector2>();
-		_pos.x -= _in_dir.x;
-		_pos.z -= _in_dir.y;
 
-		transform.LookAt(_pos);
+		if (_in_dir.magnitude > 0)
+		{
+			// 表示する
+			Color _col = m_arrow.color;
+			_col.a = 1.0f;
+			m_arrow.color = _col;
+
+			// 回転する
+			Vector3 _pos = transform.position;
+			_pos.x -= _in_dir.x;
+			_pos.z -= _in_dir.y;
+			transform.LookAt(_pos);
+		}
+		else
+		{
+			// 非表示にする
+			Color _col = m_arrow.color;
+			_col.a = 0.0f;
+			m_arrow.color = _col;
+		}
 	}
-
 }

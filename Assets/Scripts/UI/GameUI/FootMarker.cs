8eced57d972218a233684844c07108c2bc4c2e64
Mizuki_Tanisawa <wasaizumi@gmail.com>
2020-06-02 21:06:07 +0900

    足元のマーカー

---
+++ Assets/Scripts/UI/GameUI/FootMarker.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * @file	FootMarker.cs
+ * @brief	親ペンギンの足元のマーカー
+ * @auther	谷沢　瑞己
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/**
+ * @file	FootMarkerクラス
+ * @brief	親ペンギンの足元のマーカー
+ */
+public class FootMarker : MonoBehaviour
+{
+	[SerializeField, NonEditableField, Tooltip("インプット")]
+	private PlayerInput m_input = null;
+
+	public void Awake()
+	{
+		m_input = FindObjectOfType<PlayerInput>();
+	}
+
+	public void Start()
+	{
+		m_input.actions["Move"].performed += CursorRotate;
+	}
+
+	// 選んだステージ読み込む
+	private void CursorRotate(InputAction.CallbackContext _context)
+	{
+		Vector3 _pos = transform.position;
+		Vector2 _in_dir = _context.ReadValue<Vector2>();
+		_pos.x -= _in_dir.x;
+		_pos.z -= _in_dir.y;
+
+		transform.LookAt(_pos);
+	}
+
+}

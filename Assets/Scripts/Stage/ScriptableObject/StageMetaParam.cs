b0d04b3a7061f07a33b78018d64016ae89fd61df
Mizuki_Tanisawa <wasaizumi@gmail.com>
2020-06-04 16:48:53 +0900

    コメント削除忘れ

--- Assets/Scripts/Stage/ScriptableObject/StageMetaParam.cs
+++ Assets/Scripts/Stage/ScriptableObject/StageMetaParam.cs
@@ -40,7 +40,6 @@ public class StageMetaParam : ScriptableObject
 		}
 	}
 
-
 	//! 現在のエリア番号
 	public int m_current_area_index;
 	//! 現在のステージ番号
@@ -63,30 +62,4 @@ public class StageMetaParam : ScriptableObject
 	{
 		return m_levelnum_each_area[_area_index];
 	}
-
-
-	//// 以下古いシステムの残り物
-	//[Header("old system")]
-
-	////! エリアリスト
-	//[SerializeField]
-	//private AreaData[] m_area_list = new AreaData[4];
-
-	////! 現在指定されているエリアのリストを呼び出す
-	//public AreaData CurrentAreaList
-	//{
-	//	get{ return m_area_list[m_current_area_index % m_area_list.Length]; }
-	//}
-
-	////! 現在指定されているステージのデータを呼び出す
-	//public StageData CurrentLevelData
-	//{
-	//	get{ return m_area_list[m_current_area_index % m_area_list.Length].GetListItem(m_current_stage_index); }
-	//}
-
-	//// 任意のエリアを取り出す
-	//public AreaData GetAreaList(int _area_index)
-	//{
-	//	return m_area_list[_area_index % m_area_list.Length];
-	//}
 }

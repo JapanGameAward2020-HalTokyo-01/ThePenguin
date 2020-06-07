74fc001b957de7459126fdf2983297f6cebc1996
Mizuki_Tanisawa <wasaizumi@gmail.com>
2020-06-07 21:41:32 +0900

    作業途中

--- Assets/Scripts/AudioSystem/BGMSystem/BGMManager.cs
+++ Assets/Scripts/AudioSystem/BGMSystem/BGMManager.cs
@@ -3,6 +3,8 @@
  * @brief   AudioSystem機能群とそれを使うクラスに分離し、使う側のクラスをゲームオブジェクトにくっつけるという実装方針(GoF的にはBridgeパターンをイメージ)
  * @author  谷沢 瑞己
  */
+using Boo.Lang;
+using System.ComponentModel;
 using UnityEngine;
 
 /**
@@ -16,20 +18,15 @@ public class BGMManager : MonoBehaviour
 
 	//! オーディオリスト(BGM切り替え等に使用)
 	[SerializeField]
-	private AudioBGMList m_audio_list = null;
+	private BGMs m_audio_list = null;
 
 	[Header("BGM")]
 
-	//! シーン初期BGM
-	[SerializeField]
-	private AudioBGMParams m_init_bgm = null;
-	public AudioBGMParams BGMParam
-	{
-		get { return m_init_bgm; }
-	}
+	//! 操作対象オーディオソースコンポーネント
+	AudioSource m_current_source = null;
+	private List<AudioSource> m_source = null;
 
-	//! 操作対象オーディオソース
-	AudioSource m_source = null;
+	AudioBGMParams m_param;
 
 	//! 機能群
 	private AudioLoop m_loop = new AudioLoop();
@@ -40,20 +37,28 @@ public class BGMManager : MonoBehaviour
 	}
 
 	/**
-	 * @brief   初期化
+	 * @brief   BGM再生
 	 */
-	public void Start()
+	public void Play(BGMs.Index _index, int _area = 0)
 	{
-		// AudioSource作成
-		m_source = gameObject.GetComponent<AudioSource>();
-		if (m_source == null) m_source = gameObject.AddComponent<AudioSource>();
+		// AudioSourceの切り替え・作成
+		if (m_current_source != null)
+		{
+			fade(0.0f, 1.0f, m_current_source, m_param);
+			m_source.Add(m_current_source);
+		}
+		m_current_source = gameObject.GetComponent<AudioSource>();
+		if (m_current_source == null) m_current_source = gameObject.AddComponent<AudioSource>();
 
-		if(m_init_bgm == null) m_source.clip = null;
-		else  m_source.clip = m_init_bgm.Clip;
+		// インデックスからBGMファイル固有パラメータ取得
+		m_param = m_audio_list.SelectBGM(_index);
+		if (m_param == null) m_current_source.clip = null;
+		else m_current_source.clip = m_param.Clip;
 
-		m_source.outputAudioMixerGroup = m_audio_list.BGMMixer;
-		m_source.loop = true;
-		m_source.Play();
+		// 初期化
+		m_current_source.outputAudioMixerGroup = m_audio_list.BGMMixer;
+		m_current_source.loop = true;
+		m_current_source.Play(1.0f);
 	}
 
 	/**
@@ -61,17 +66,15 @@ public class BGMManager : MonoBehaviour
 	 */
 	public void FixedUpdate()
 	{
-		m_loop.OnUpdate(m_source, m_init_bgm);
+		m_loop.OnUpdate(m_current_source, m_init_bgm);
 	}
-
 
 	/**
 	 * @brief   フェード機能
 	 */
-	 public void fade(float _end_value, float _proc_sec)
+	 public void fade(float _end_value, float _proc_sec, AudioSource _source, AudioBGMParams _param)
 	{
-		m_fade.Set(m_source, _end_value, _proc_sec);
+		m_fade.Set(_source, _end_value, _proc_sec);
 		StartCoroutine("m_fade.FadeUpdate");
 	}
-
 }

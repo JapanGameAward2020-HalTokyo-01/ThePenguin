0ad6188c7d998e2ead05d59f4042b9c0df27cad5
Crocell94 <spartan202@gmail.com>
2020-06-11 18:09:04 +0900

    念の為threadの親が死んだらぬけるように修正。

--- Assets/Scripts/UI/GameMain/GameUI.cs
+++ Assets/Scripts/UI/GameMain/GameUI.cs
@@ -166,7 +166,7 @@ public class GameUI : MonoBehaviour
         for (float i = 0; i <= 1; )
         {
             Debug.Log("Accel L");
-            if (!m_LAccel)
+            if (!m_LAccel | !this)
             {
                 yield break;
             }
@@ -188,7 +188,7 @@ public class GameUI : MonoBehaviour
         for (float i = 0; i <= 1; )
         {
             Debug.Log("Accel R");
-            if (!m_RAccel)
+            if (!m_RAccel | !this)
             {
                 yield break;
             }
@@ -324,7 +324,7 @@ public class GameUI : MonoBehaviour
         for (; i >= 0; )
         {
             Debug.Log("Decel L");
-            if (!m_LDecel)
+            if (!m_LDecel | !this)
             {
                 yield break;
             }
@@ -348,7 +348,7 @@ public class GameUI : MonoBehaviour
         for (; i >= 0; )
         {
             Debug.Log("Decel R");
-            if (!m_RDecel)
+            if (!m_RDecel | !this)
             {
                 yield break;
             }
